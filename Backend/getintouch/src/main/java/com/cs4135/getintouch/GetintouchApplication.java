86f3215af2d39f6648f863bf199a7f509a562e78
Aron Calvert <calvertaron@gmail.com>
2026-02-11 10:55:43 +0000

    Backend commit

---
+++ Backend/getintouch/src/main/java/com/cs4135/getintouch/GetintouchApplication.java
@@ -0,0 +1,13 @@
+package com.cs4135.getintouch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetintouchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetintouchApplication.class, args);
+	}
+
+}
---
+++ Backend/getintouch/src/test/java/com/cs4135/getintouch/GetintouchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs4135.getintouch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetintouchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

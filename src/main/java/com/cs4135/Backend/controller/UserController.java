50378c2a05be1b664b30226bd5b45d188d63788e
VenturiVen <gingeroshea443@gmail.com>
2026-04-15 17:44:23 +0100

    Endpoint fix

--- src/main/java/com/cs4135/Backend/controller/UserController.java
+++ src/main/java/com/cs4135/Backend/controller/UserController.java
@@ -42,7 +42,7 @@ public class UserController {
   }
 
   @PreAuthorize("hasRole('ADMIN')")
-  @DeleteMapping("/{id}")
+  @DeleteMapping("/delete/{id}")
   public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
     userService.deleteUser(id);
     return ResponseEntity.noContent().build();
